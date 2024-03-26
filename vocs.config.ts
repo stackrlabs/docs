75880be30d4a9cd6f9188f9c4f8574556560b1ec
Aashutosh Rathi <aashutosh@stackrlabs.xyz>
2024-03-26 13:21:01 +0530

    docs: init beta version

--- vocs.config.ts
+++ vocs.config.ts
@@ -20,80 +20,288 @@ export default defineConfig({
   title: "Stackr",
   sidebar: [
     {
-      text: "Setup",
+      text: "Micro-Rollup",
       items: [
         {
-          text: "Dev Environment",
-          link: "/setup/dev-environment",
+          text: "Introduction",
+          link: "/micro-rollup/introduction",
         },
         {
-          text: "SDK Access",
-          link: "/setup/SDK-access",
+          text: "Motivation",
+          collapsed: true,
+          items: [
+            {
+              text: "App-specific Rollups",
+              link: "/micro-rollup/motivation/app-specific-rollups",
+            },
+            {
+              text: "Sufficient Decentralization",
+              link: "/micro-rollup/motivation/sufficient-decentralization",
+            },
+            {
+              text: "Single App Rollups",
+              link: "/micro-rollup/motivation/single-app-rollups",
+            },
+          ],
         },
         {
-          text: "Stackr Testnet",
-          link: "/config/configurations",
+          text: "Architecture",
+          collapsed: true,
+          items: [
+            {
+              text: "Overview",
+              link: "/micro-rollup/architecture/overview",
+            },
+            {
+              text: "Execution Layer",
+              link: "/micro-rollup/architecture/execution-layer",
+            },
+            {
+              text: "Verification Layer",
+              link: "/micro-rollup/architecture/verification-layer",
+            },
+          ],
+        },
+        {
+          text: "Economics",
+          link: "/micro-rollup/economics",
         },
       ],
     },
     {
-      text: "0-to-1",
+      text: "Zero to One",
       items: [
         {
-          text: "Start Here",
-          link: "/start/start-here",
+          text: "Getting Started",
+          link: "/zero-to-one/getting-started",
         },
         {
-          text: "Building First Rollup",
-          link: "/start/building-first-rollup",
+          text: "Build your first Micro-rollup",
+          link: "/zero-to-one/build-your-first-micro-rollup",
         },
         {
-          text: "Running First Rollup",
-          link: "/start/running-first-rollup",
+          text: "Run it",
+          link: "/zero-to-one/run-it",
         },
       ],
     },
     {
-      text: "Develop",
+      text: "Framework",
       items: [
         {
-          text: "Before You Begin",
-          link: "/develop/before-you-begin",
+          text: "Action",
+          link: "/framework/action",
         },
         {
-          text: "State",
-          link: "/develop/state",
+          text: "Block",
+          link: "/framework/block.mdx",
         },
         {
-          text: "Rollup State",
-          link: "/develop/rollup-state",
+          text: "State Machine",
+          collapsed: true,
+          items: [
+            {
+              text: "State",
+              link: "/framework/state-machine/state",
+            },
+            {
+              text: "Reducers",
+              link: "/framework/state-machine/reducers",
+            },
+          ],
         },
         {
-          text: "State Transition Function",
-          link: "/develop/state-transition-function",
+          text: "Sequencer",
+          collapsed: true,
+          items: [
+            {
+              text: "Builder",
+              link: "/framework/sequencer/builder",
+            },
+            {
+              text: "Proposer",
+              link: "/framework/sequencer/proposer",
+            },
+          ],
         },
         {
-          text: "Almost Micro Rollup",
-          link: "/develop/almost-micro-rollup",
+          text: "Executor",
+          link: "/framework/executor",
+        },
+        { text: "Events", link: "/framework/events" },
+      ],
+    },
+    {
+      text: "Build",
+      items: [
+        {
+          text: "Setup",
+          collapsed: true,
+          items: [
+            { text: "Getting Started", link: "/build/setup/getting-started" },
+            { text: "Config", link: "/build/setup/config" },
+          ],
         },
         {
-          text: "Stackr Config",
-          link: "/develop/stackr-config",
+          text: "Develop",
+          collapsed: true,
+          items: [
+            {
+              text: "Init",
+              link: "/build/develop/init",
+            },
+            {
+              text: "Register",
+              link: "/build/develop/register",
+            },
+            {
+              text: "Build",
+              link: "/build/develop/build",
+            },
+            {
+              text: "Deploy",
+              link: "/build/develop/deploy",
+            },
+          ],
         },
         {
-          text: "Builder Sequencer",
-          link: "/develop/builder-sequencer",
+          text: "Plugins",
+          collapsed: true,
+          items: [
+            {
+              text: "Playground",
+              link: "/build/plugins/playground",
+            },
+            {
+              text: "Make your own",
+              link: "/build/plugins/make-your-own",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Security",
+      items: [
+        {
+          text: "Protocol",
+          link: "/security/protocol",
         },
         {
-          text: "Micro Rollup",
-          link: "/develop/micro-rollup",
+          text: "Data Settlement",
+          collapsed: true,
+          items: [
+            {
+              text: "Off-chain data",
+              link: "/security/data-settlement/Off-chain-storage",
+            },
+            {
+              text: "On-chain data",
+              link: "/security/data-settlement/On-chain-storage",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Guides",
+      items: [
+        {
+          text: "What to Build?",
+          link: "/guides/what-to-build",
         },
         {
-          text: "Micro Rollup Utilities",
-          link: "/develop/micro-rollup-utilities",
+          text: "Tutorials",
+          collapsed: true,
+          items: [
+            {
+              text: "Token Transfer",
+              link: "/guides/tutorials/token-transfer",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Stale",
+      collapsed: true,
+      items: [
+        {
+          text: "Setup",
+          items: [
+            {
+              text: "Dev Environment",
+              link: "/stale/setup/dev-environment",
+            },
+            {
+              text: "SDK Access",
+              link: "/stale/setup/SDK-access",
+            },
+            {
+              text: "Stackr Testnet",
+              link: "/stale/config/configurations",
+            },
+          ],
+        },
+        {
+          text: "0-to-1",
+          items: [
+            {
+              text: "Start Here",
+              link: "/stale/start/start-here",
+            },
+            {
+              text: "Building First Rollup",
+              link: "/stale/start/building-first-rollup",
+            },
+            {
+              text: "Running First Rollup",
+              link: "/stale/start/running-first-rollup",
+            },
+          ],
+        },
+        {
+          text: "Develop",
+          items: [
+            {
+              text: "Before You Begin",
+              link: "/stale/develop/before-you-begin",
+            },
+            {
+              text: "State",
+              link: "/stale/develop/state",
+            },
+            {
+              text: "Rollup State",
+              link: "/stale/develop/rollup-state",
+            },
+            {
+              text: "State Transition Function",
+              link: "/stale/develop/state-transition-function",
+            },
+            {
+              text: "Almost Micro Rollup",
+              link: "/stale/develop/almost-micro-rollup",
+            },
+            {
+              text: "Stackr Config",
+              link: "/stale/develop/stackr-config",
+            },
+            {
+              text: "Builder Sequencer",
+              link: "/stale/develop/builder-sequencer",
+            },
+            {
+              text: "Micro Rollup",
+              link: "/stale/develop/micro-rollup",
+            },
+            {
+              text: "Micro Rollup Utilities",
+              link: "/stale/develop/micro-rollup-utilities",
+            },
+          ],
         },
       ],
     },
   ],
-
 });
