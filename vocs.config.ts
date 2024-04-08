233ce08648a447e6492aabc5110d42418e06f00b
Kautuk Kundan <kautuk@stackrlabs.xyz>
2024-04-08 13:55:50 +0530

    feat: added counter tutorial and disabled other pages

--- vocs.config.ts
+++ vocs.config.ts
@@ -153,126 +153,156 @@ export default defineConfig({
       ],
     },
     {
+      text: "Guides",
+      collapsed: true,
+      items: [
+        {
+          text: "Check your Binaries",
+          link: "/guides/checking-your-binaries",
+        },
+        {
+          text: "Tutorials",
+          collapsed: true,
+          items: [
+            {
+              text: "Counter MRU",
+              link: "/guides/tutorials/counter",
+            },
+          ],
+        },
+      ],
+    },
+    {
       text: "Framework",
       collapsed: true,
       items: [
         {
-          text: "State Machine",
-          collapsed: true,
-          items: [
-            {
-              text: "Introduction",
-              link: "/framework/state-machine/introduction",
-            },
-            {
-              text: "State",
-              link: "/framework/state-machine/state",
-            },
-            {
-              text: "Reducers",
-              link: "/framework/state-machine/reducers",
-            },
-          ],
+          text: "Coming Soon",
+          link: "/framework/placeholder",
         },
-        {
-          text: "Action",
-          collapsed: true,
-          items: [
-            {
-              text: "Introduction",
-              link: "/framework/action/introduction",
-            },
-            {
-              text: "Action Schema",
-              link: "/framework/action/action-schema",
-            },
-            {
-              text: "Lifecycle",
-              link: "/framework/action/lifecycle",
-            },
-            {
-              text: "Execution Status",
-              link: "/framework/action/execution-status",
-            },
-            {
-              text: "Confirmation Status",
-              link: "/framework/action/confirmation-status",
-            },
-          ],
-        },
-        {
-          text: "Block",
-          link: "/framework/block.mdx",
-        },
-        {
-          text: "Sequencer",
-          collapsed: true,
-          items: [
-            {
-              text: "Builder",
-              link: "/framework/sequencer/builder",
-            },
-            {
-              text: "Proposer",
-              link: "/framework/sequencer/proposer",
-            },
-          ],
-        },
-        {
-          text: "Executor",
-          link: "/framework/executor",
-        },
-        { text: "Events", link: "/framework/events" },
       ],
+      // items: [
+      //   {
+      //     text: "State Machine",
+      //     collapsed: true,
+      //     items: [
+      //       {
+      //         text: "Introduction",
+      //         link: "/framework/state-machine/introduction",
+      //       },
+      //       {
+      //         text: "State",
+      //         link: "/framework/state-machine/state",
+      //       },
+      //       {
+      //         text: "Reducers",
+      //         link: "/framework/state-machine/reducers",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     text: "Action",
+      //     collapsed: true,
+      //     items: [
+      //       {
+      //         text: "Introduction",
+      //         link: "/framework/action/introduction",
+      //       },
+      //       {
+      //         text: "Action Schema",
+      //         link: "/framework/action/action-schema",
+      //       },
+      //       {
+      //         text: "Lifecycle",
+      //         link: "/framework/action/lifecycle",
+      //       },
+      //       {
+      //         text: "Execution Status",
+      //         link: "/framework/action/execution-status",
+      //       },
+      //       {
+      //         text: "Confirmation Status",
+      //         link: "/framework/action/confirmation-status",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     text: "Block",
+      //     link: "/framework/block.mdx",
+      //   },
+      //   {
+      //     text: "Sequencer",
+      //     collapsed: true,
+      //     items: [
+      //       {
+      //         text: "Builder",
+      //         link: "/framework/sequencer/builder",
+      //       },
+      //       {
+      //         text: "Proposer",
+      //         link: "/framework/sequencer/proposer",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     text: "Executor",
+      //     link: "/framework/executor",
+      //   },
+      //   { text: "Events", link: "/framework/events" },
+      // ],
     },
     {
       text: "Build",
       collapsed: true,
       items: [
         {
-          text: "Setup",
-          collapsed: true,
-          items: [
-            { text: "Getting Started", link: "/build/setup/getting-started" },
-            { text: "Config", link: "/build/setup/config" },
-          ],
+          text: "Coming Soon",
+          link: "/build/placeholder",
         },
-        {
-          text: "Develop",
-          collapsed: true,
-          items: [
-            {
-              text: "Init",
-              link: "/build/develop/init",
-            },
-            {
-              text: "Register",
-              link: "/build/develop/register",
-            },
-            {
-              text: "Build",
-              link: "/build/develop/build",
-            },
-            {
-              text: "Deploy",
-              link: "/build/develop/deploy",
-            },
-          ],
-        },
-        {
-          text: "Plugins",
-          collapsed: true,
-          items: [
-            {
-              text: "Playground",
-              link: "/build/plugins/playground",
-            },
-            {
-              text: "Make your own",
-              link: "/build/plugins/make-your-own",
-            },
-          ],
-        },
+        // {
+        //   text: "Setup",
+        //   collapsed: true,
+        //   items: [
+        //     { text: "Getting Started", link: "/build/setup/getting-started" },
+        //     { text: "Config", link: "/build/setup/config" },
+        //   ],
+        // },
+        // {
+        //   text: "Develop",
+        //   collapsed: true,
+        //   items: [
+        //     {
+        //       text: "Init",
+        //       link: "/build/develop/init",
+        //     },
+        //     {
+        //       text: "Register",
+        //       link: "/build/develop/register",
+        //     },
+        //     {
+        //       text: "Build",
+        //       link: "/build/develop/build",
+        //     },
+        //     {
+        //       text: "Deploy",
+        //       link: "/build/develop/deploy",
+        //     },
+        //   ],
+        // },
+        // {
+        //   text: "Plugins",
+        //   collapsed: true,
+        //   items: [
+        //     {
+        //       text: "Playground",
+        //       link: "/build/plugins/playground",
+        //     },
+        //     {
+        //       text: "Make your own",
+        //       link: "/build/plugins/make-your-own",
+        //     },
+        //   ],
+        // },
       ],
     },
     {
@@ -300,26 +330,6 @@ export default defineConfig({
       ],
     },
     {
-      text: "Guides",
-      collapsed: true,
-      items: [
-        {
-          text: "Check your Binaries",
-          link: "/guides/checking-your-binaries",
-        },
-        {
-          text: "Tutorials",
-          collapsed: true,
-          items: [
-            {
-              text: "Token Transfer",
-              link: "/guides/tutorials/token-transfer",
-            },
-          ],
-        },
-      ],
-    },
-    {
       text: "References",
       collapsed: true,
       items: [
@@ -329,86 +339,86 @@ export default defineConfig({
         },
       ],
     },
-    {
-      text: "Stale",
-      collapsed: true,
-      items: [
-        {
-          text: "Setup",
-          items: [
-            {
-              text: "Dev Environment",
-              link: "/stale/setup/dev-environment",
-            },
-            {
-              text: "SDK Access",
-              link: "/stale/setup/SDK-access",
-            },
-            {
-              text: "Stackr Testnet",
-              link: "/stale/config/configurations",
-            },
-          ],
-        },
-        {
-          text: "0-to-1",
-          items: [
-            {
-              text: "Start Here",
-              link: "/stale/start/start-here",
-            },
-            {
-              text: "Building First Rollup",
-              link: "/stale/start/building-first-rollup",
-            },
-            {
-              text: "Running First Rollup",
-              link: "/stale/start/running-first-rollup",
-            },
-          ],
-        },
-        {
-          text: "Develop",
-          items: [
-            {
-              text: "Before You Begin",
-              link: "/stale/develop/before-you-begin",
-            },
-            {
-              text: "State",
-              link: "/stale/develop/state",
-            },
-            {
-              text: "Rollup State",
-              link: "/stale/develop/rollup-state",
-            },
-            {
-              text: "State Transition Function",
-              link: "/stale/develop/state-transition-function",
-            },
-            {
-              text: "Almost Micro Rollup",
-              link: "/stale/develop/almost-micro-rollup",
-            },
-            {
-              text: "Stackr Config",
-              link: "/stale/develop/stackr-config",
-            },
-            {
-              text: "Builder Sequencer",
-              link: "/stale/develop/builder-sequencer",
-            },
-            {
-              text: "Micro Rollup",
-              link: "/stale/develop/micro-rollup",
-            },
-            {
-              text: "Micro Rollup Utilities",
-              link: "/stale/develop/micro-rollup-utilities",
-            },
-          ],
-        },
-      ],
-    },
+    // {
+    //   text: "Stale",
+    //   collapsed: true,
+    //   items: [
+    //     {
+    //       text: "Setup",
+    //       items: [
+    //         {
+    //           text: "Dev Environment",
+    //           link: "/stale/setup/dev-environment",
+    //         },
+    //         {
+    //           text: "SDK Access",
+    //           link: "/stale/setup/SDK-access",
+    //         },
+    //         {
+    //           text: "Stackr Testnet",
+    //           link: "/stale/config/configurations",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       text: "0-to-1",
+    //       items: [
+    //         {
+    //           text: "Start Here",
+    //           link: "/stale/start/start-here",
+    //         },
+    //         {
+    //           text: "Building First Rollup",
+    //           link: "/stale/start/building-first-rollup",
+    //         },
+    //         {
+    //           text: "Running First Rollup",
+    //           link: "/stale/start/running-first-rollup",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       text: "Develop",
+    //       items: [
+    //         {
+    //           text: "Before You Begin",
+    //           link: "/stale/develop/before-you-begin",
+    //         },
+    //         {
+    //           text: "State",
+    //           link: "/stale/develop/state",
+    //         },
+    //         {
+    //           text: "Rollup State",
+    //           link: "/stale/develop/rollup-state",
+    //         },
+    //         {
+    //           text: "State Transition Function",
+    //           link: "/stale/develop/state-transition-function",
+    //         },
+    //         {
+    //           text: "Almost Micro Rollup",
+    //           link: "/stale/develop/almost-micro-rollup",
+    //         },
+    //         {
+    //           text: "Stackr Config",
+    //           link: "/stale/develop/stackr-config",
+    //         },
+    //         {
+    //           text: "Builder Sequencer",
+    //           link: "/stale/develop/builder-sequencer",
+    //         },
+    //         {
+    //           text: "Micro Rollup",
+    //           link: "/stale/develop/micro-rollup",
+    //         },
+    //         {
+    //           text: "Micro Rollup Utilities",
+    //           link: "/stale/develop/micro-rollup-utilities",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
   ],
 });
